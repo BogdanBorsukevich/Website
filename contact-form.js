be9f258328515863959bdd7a729f80a610b11e0b
Bogdan Borsukevich <borsukevich.bogdan173@gmail.com>
2025-06-30 01:10:16 +0300

    Adapt

---
+++ contact-form.js
@@ -0,0 +1,19 @@
+  document.getElementById('burger').addEventListener('click', function () {
+    document.getElementById('mobileMenu').classList.toggle('show');
+  });
+
+  const items = document.querySelectorAll('.animate-on-scroll');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }, {
+    threshold: 0.3
+  });
+
+  items.forEach(item => observer.observe(item));
